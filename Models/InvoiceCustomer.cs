0ace013fba62567f92642cd9943aaff5e74fab0d
salazar <WussubiNinja@live.com>
2020-12-29 20:13:52 +1000

    Added extra fields to InvoiceBusiness and InvoiceCustomer models

--- Models/InvoiceCustomer.cs
+++ Models/InvoiceCustomer.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System;
 using System.Collections.Generic;
 
 namespace InventoryControlSystem.Models
@@ -11,5 +12,17 @@ namespace InventoryControlSystem.Models
         public string Id { get; set; }
         public string ID { get; set; }
         public List<Order> CustomerOrders { get; set; } = new List<Order>();
+        public Order Order { get; set; }
+        public string SenderName { get; set; }
+        public string SenderAddress { get; set; }
+        public string SenderEmail { get; set; }
+        public string SenderPhone { get; set; }
+        public string RecieverName { get; set; }
+        public string RecieverAddress { get; set; }
+        public string RecieverEmail { get; set; }
+        public string RecieverPhone { get; set; }
+        public DateTime Date { get; set; }
+        public List<Product> Products { get; set; }
+        public double TotalCost { get; set; }
     }
 }
