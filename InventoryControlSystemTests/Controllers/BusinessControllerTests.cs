835de0616da9150e815884eeac70cc14dc319479
n9969071 <48539372+Jonathan-ol-Salazar@users.noreply.github.com>
2021-02-25 15:54:03 +1000

    Added more tests for BusinessController

--- InventoryControlSystemTests/Controllers/BusinessControllerTests.cs
+++ InventoryControlSystemTests/Controllers/BusinessControllerTests.cs
@@ -16,32 +16,91 @@ namespace InventoryControlSystemTests.Controllers
     [TestClass]
     public class BusinessControllerTests
     {
-        [TestMethod]
-        public async Task Index_WhenCalled_ReturnAllBusinesses()
+        Business business;
+        List<Business> businesses;
+        Mock<IBusinessRepository> businessRepo;
+        Mock<IOrderRepository> orderRepo;
+        BusinessController controller;
+
+        [TestInitialize]
+        public void SetUp()
         {
             // Arrange
-            var businesses = new List<Business>
+            business = new Business
             {
-                new Business{
-                    ID = "",
-                    Id = "",
-                    Name = "",
-                    Email = "",
-                    Address = "",
-                    Phone = 000,
-                    Selected = false
-                }
+                ID = "",
+                Id = "",
+                Name = "",
+                Email = "",
+                Address = "",
+                Phone = 000,
+                Selected = false
             };
-            var businessRepo = new Mock<IBusinessRepository>();
-            businessRepo.Setup(r => r.GetAllBusinesss()).ReturnsAsync(businesses);
-            var orderRepo = new Mock<IOrderRepository>();
-            
-            var controller = new BusinessController(businessRepo.Object, orderRepo.Object); ;
 
+            businesses = new List<Business>
+            {
+                business
+            };
+            businessRepo = new Mock<IBusinessRepository>();
+            businessRepo.Setup(r => r.GetAllBusinesss()).ReturnsAsync(businesses);
+            orderRepo = new Mock<IOrderRepository>();
+
+            controller = new BusinessController(businessRepo.Object, orderRepo.Object); ;
+
+        }
+
+        [TestMethod]
+        public async Task Index_WhenCalled_ReturnModel()
+        {      
             // Act
             var result = await controller.Index() as ViewResult;
+            var model = result.Model;
+
             // Assert
             Assert.IsNotNull(result);
+            CollectionAssert.AreEquivalent(businesses,(System.Collections.ICollection)model);
+        }
+
+        [TestMethod]
+        public async Task Index_WhenCalled_ReturnViewResult()
+        {
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+        }
+
+        [TestMethod]
+        public async Task Details_WhenCalledWithExistingID_ReturnBusiness()
+        {
+            // Arrange
+            businessRepo.Setup(r => r.GetBusiness(business.Id)).ReturnsAsync(business);
+
+            controller = new BusinessController(businessRepo.Object, orderRepo.Object);
+
+            // Act
+            var result = await controller.Details(business.Id) as ViewResult;
+            var model = result.Model;
+
+            // Assert
+            Assert.IsNotNull(model);
+            Assert.AreEqual(business, model);
+        }
+
+        [DataTestMethod]
+        [DataRow("9")]
+        public async Task Details_WhenCalledWithNonExistingID_ReturnNotFound(string id)
+        {
+            // Arrange
+            businessRepo.Setup(r => r.GetBusiness(id));
+            controller = new BusinessController(businessRepo.Object,orderRepo.Object);
+
+            // Act
+            var result = await controller.Details(id);
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
     }
 }
