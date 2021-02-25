00c7138cbee5f9bb33e7b738747720b50bbdc022
n9969071 <48539372+Jonathan-ol-Salazar@users.noreply.github.com>
2021-02-25 14:02:02 +1000

    Added tests for business controller

---
+++ InventoryControlSystemTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,76 @@
+﻿using InventoryControlSystem.Controllers;
+using InventoryControlSystem.Models;
+using InventoryControlSystem.Repositories.Users;
+using InventoryControlSystem.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryControlSystemTests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTests
+    {
+        User user;
+        Mock<IUserRepository> repo;
+        AccountController controller;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Arrange
+            user = new User
+            {
+                FirstName = "",
+                LastName = "",
+                Address = "",
+                DOB = "",
+                Email = "f",
+                Phone = "",
+                Picture = "",
+                Role = "",
+                Auth0ID = "",
+                ID = "",
+                Id = ""
+            };
+            repo = new Mock<IUserRepository>();
+            repo.Setup(r => r.GetUser(user.ID)).ReturnsAsync(user);
+
+            controller = new AccountController(repo.Object);
+        }
+        //[TestMethod]
+        //public async Task Index_WhenCalled_ReturnViewModel()
+        //{
+        //    // Act
+        //    var result = await controller.Index() as ViewResult;
+        //    var model = result.Model as UserViewModel;
+
+        //    // Assert
+        //    Assert.IsNotNull(result);
+        //    Assert.AreEqual(user, model);
+        //}
+
+        //[TestMethod]
+        //public  void Index_WhenCalled_ReturnView()
+        //{
+        //    // Act
+        //    var result =  controller.Index();
+
+        //    // Assert
+        //    Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+        //}
+
+        [TestMethod]
+        public void yeet()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
+
---
+++ InventoryControlSystemTests/Controllers/BusinessControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using InventoryControlSystem.Controllers;
+using InventoryControlSystem.Models;
+using InventoryControlSystem.Repositories.Businesses;
+using InventoryControlSystem.Repositories.Orders;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryControlSystemTests.Controllers
+{
+    [TestClass]
+    public class BusinessControllerTests
+    {
+        [TestMethod]
+        public async Task Index_WhenCalled_ReturnAllBusinesses()
+        {
+            // Arrange
+            var businesses = new List<Business>
+            {
+                new Business{
+                    ID = "",
+                    Id = "",
+                    Name = "",
+                    Email = "",
+                    Address = "",
+                    Phone = 000,
+                    Selected = false
+                }
+            };
+            var businessRepo = new Mock<IBusinessRepository>();
+            businessRepo.Setup(r => r.GetAllBusinesss()).ReturnsAsync(businesses);
+            var orderRepo = new Mock<IOrderRepository>();
+            
+            var controller = new BusinessController(businessRepo.Object, orderRepo.Object); ;
+
+            // Act
+            var result = await controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
